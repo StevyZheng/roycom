e005bd87d294f0f30166fc61416413b1f8514339
664553716@qq.com <jqy951753>
2018-11-06 00:00:12 +0800

    Initial commit

---
+++ manager/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+	user_name = models.CharField(max_length=24, unique=True, verbose_name="用户名")
+	password = models.CharField(_('password'), max_length=36, verbose_name="密码")
+	sex = models.CharField(max_length=1, null=True, verbose_name="性别")
+	role = models.ForeignKey(Role, on_delete=models.CASCADE, verbose_name="角色")
+
+
+class Role(models.Model):
+	role_name = models.CharField(max_length=24, unique=True)
+
+
+class RoleToPrivilege(models.Model):
+	role = models.ForeignKey(Role, on_delete=models.CASCADE, verbose_name="角色")
+	privilege = models.CharField(max_length=512, verbose_name="权限")
